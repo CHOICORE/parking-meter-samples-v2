71b828ce43cf77f0388142e9d8f4c6480f532521
CHOICORE <choijaehyeong93@gmail.com>
2025-03-18 18:34:42 +0900

    refactor: add id property to MeteringStrategy and improve class structure for time slot strategies

--- src/main/kotlin/me/choicore/samples/operation/domain/MeteringStrategy.kt
+++ src/main/kotlin/me/choicore/samples/operation/domain/MeteringStrategy.kt
@@ -4,31 +4,39 @@ import java.time.DayOfWeek
 import java.time.LocalDate
 
 sealed interface MeteringStrategy : Meter {
+    val id: Long
     val lotId: Long
 
     fun applies(measuredOn: LocalDate): Boolean
 
+    sealed interface TimeSlotMeteringStrategy : MeteringStrategy {
+        val timeSlotMeter: TimeSlotMeter
+    }
+
     sealed class AllDayMeteringStrategy(
+        override val id: Long,
         override val lotId: Long,
-        open val timeSlotMeter: TimeSlotMeter,
-    ) : MeteringStrategy {
+        override val timeSlotMeter: TimeSlotMeter,
+    ) : TimeSlotMeteringStrategy {
         override fun measure(measurand: Measurand): List<Measurement> = this.timeSlotMeter.measure(measurand = measurand)
     }
 
     data class SpecifiedDateMeteringStrategy(
+        override val id: Long = 0,
         override val lotId: Long,
         override val timeSlotMeter: TimeSlotMeter,
         val specifiedDate: LocalDate,
-    ) : AllDayMeteringStrategy(lotId = lotId, timeSlotMeter = timeSlotMeter) {
+    ) : AllDayMeteringStrategy(id = id, lotId = lotId, timeSlotMeter = timeSlotMeter) {
         override fun applies(measuredOn: LocalDate): Boolean = measuredOn == this.specifiedDate
     }
 
     data class DayOfWeekMeteringStrategy(
+        override val id: Long = 0,
         override val lotId: Long,
         override val timeSlotMeter: TimeSlotMeter,
         val dayOfWeek: DayOfWeek,
         val effectiveDate: LocalDate,
-    ) : AllDayMeteringStrategy(lotId = lotId, timeSlotMeter = timeSlotMeter) {
+    ) : AllDayMeteringStrategy(id = id, lotId = lotId, timeSlotMeter = timeSlotMeter) {
         override fun applies(measuredOn: LocalDate): Boolean = measuredOn.dayOfWeek == this.dayOfWeek && measuredOn >= this.effectiveDate
     }
 }

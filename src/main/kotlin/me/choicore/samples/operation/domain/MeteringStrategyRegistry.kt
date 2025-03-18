f4acef10a1cdcf7a6d59e1ad5df2408e8bd74015
CHOICORE <choijaehyeong93@gmail.com>
2025-03-18 17:05:49 +0900

    refactor: improve clarity in getMeteringStrategy by enhancing parameter naming and simplifying logic

--- src/main/kotlin/me/choicore/samples/operation/domain/MeteringStrategyRegistry.kt
+++ src/main/kotlin/me/choicore/samples/operation/domain/MeteringStrategyRegistry.kt
@@ -1,36 +1,38 @@
 package me.choicore.samples.operation.domain
 
+import me.choicore.samples.operation.domain.MeteringStrategy.DayOfWeekMeteringStrategy
+import me.choicore.samples.operation.domain.MeteringStrategy.SpecifiedDateMeteringStrategy
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.util.concurrent.ConcurrentHashMap
 
 data class MeteringStrategyRegistry(
-    private val dayOfWeekStrategies: List<MeteringStrategy.DayOfWeekMeteringStrategy>,
-    private val specificDateStrategies: List<MeteringStrategy.SpecifiedDateMeteringStrategy>,
+    private val dayOfWeekStrategies: List<DayOfWeekMeteringStrategy>,
+    private val specificDateStrategies: List<SpecifiedDateMeteringStrategy>,
 ) : Meter {
-    private val daysOfWeek: Map<DayOfWeek, List<MeteringStrategy.DayOfWeekMeteringStrategy>> =
-        dayOfWeekStrategies
-            .groupBy { it.dayOfWeek }
-            .mapValues { (_, strategies: List<MeteringStrategy.DayOfWeekMeteringStrategy>) ->
-                strategies.sortedByDescending { it.effectiveDate }
+    private val daysOfWeek: Map<DayOfWeek, List<DayOfWeekMeteringStrategy>> =
+        this.dayOfWeekStrategies
+            .groupBy(DayOfWeekMeteringStrategy::dayOfWeek)
+            .mapValues { (_, strategies: List<DayOfWeekMeteringStrategy>) ->
+                strategies.sortedByDescending(DayOfWeekMeteringStrategy::effectiveDate)
             }
 
-    private val specifies: Map<LocalDate, MeteringStrategy.SpecifiedDateMeteringStrategy> =
-        specificDateStrategies.associateBy { it.specifiedDate }
+    private val specifies: Map<LocalDate, SpecifiedDateMeteringStrategy> =
+        this.specificDateStrategies.associateBy(SpecifiedDateMeteringStrategy::specifiedDate)
 
     private val cache = ConcurrentHashMap<LocalDate, MeteringStrategy?>()
 
-    override fun measure(measurand: Measurand): List<Measurement> = getMeteringStrategy(measurand.measureOn).measure(measurand)
+    override fun measure(measurand: Measurand): List<Measurement> =
+        getMeteringStrategy(measuredOn = measurand.measureOn).measure(
+            measurand = measurand,
+        )
 
     fun getMeteringStrategy(measuredOn: LocalDate): MeteringStrategy {
-        return cache.computeIfAbsent(measuredOn) { date ->
-            specifies[date]?.let {
-                return@computeIfAbsent it
-            }
-
-            val applicableStrategies = daysOfWeek[date.dayOfWeek]
-            if (applicableStrategies != null) {
-                for (strategy in applicableStrategies) {
+        return this.cache.computeIfAbsent(measuredOn) { date ->
+            this.specifies[date]?.let { return@computeIfAbsent it }
+            val strategies: List<DayOfWeekMeteringStrategy>? = this.daysOfWeek[date.dayOfWeek]
+            if (strategies != null) {
+                for (strategy: DayOfWeekMeteringStrategy in strategies) {
                     if (strategy.applies(date)) {
                         return@computeIfAbsent strategy
                     }
